41421cc62d3a7ff61b7edc8282721efa60522dc8
Miguel Ruiz <524594999+devMiguelRuiz@users.noreply.github.com>
2019-02-08 22:28:05 -0600

    Remove unused usings

--- CoreApp/Controllers/HomeController.cs
+++ CoreApp/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CoreApp.Models;
 using Microsoft.AspNetCore.Mvc;
-using CoreApp.Models;
+using System.Diagnostics;
 
 namespace CoreApp.Controllers
 {
--- CoreApp/Models/ErrorViewModel.cs
+++ CoreApp/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace CoreApp.Models
 {
     public class ErrorViewModel
